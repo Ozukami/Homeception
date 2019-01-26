bf1a1acd86fdd4a4519f48b24762cc5211113ab4
Julian Goldie <julian.goldie@epitech.eu>
2019-01-26 21:54:39 +0300

    dat fucking door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public GameObject Target;
-    private int _keyNumber;
+    // public GameObject Target;
+    // private int _keyNumber;
     private bool _open = true;
 
     private GameObject _key;
 
+    private bool _isOpen = false;
+    private float _rotZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject cpy;
+        // GameObject cpy;
 
-        if (cpy = Target.transform.Find("Key").gameObject)
-        {
-            _open = false;
-            _key = cpy;
-        }
+        // if (cpy = Target.transform.Find("Key").gameObject)
+        // {
+        //     _open = false;
+        //     _key = cpy;
+        // }
+        _rotZ = transform.localEulerAngles.z;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("transform;localEuler.z = " + transform.localRotation.z);
+        Debug.Log("Rot = " + _rotZ + 90);
+        if (_isOpen && transform.localRotation.z > _rotZ - 0.5f) {
+            Debug.Log(_rotZ);
 
+            // transform.Rotate(transform.rotation.x, transform.rotation.y, transform.)
+
+            // Quaternion newRotation = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+
+            // Debug.Log("new rot => x: " + newRotation.x + "\ny: " + newRotation.y + "\nz: " + newRotation.z);
+            // Debug.Log("x: " + transform.rotation.x + "\ny: " + transform.rotation.y + "\nz: " + transform.rotation.z);
+
+            // transform.rotation = Quaternion.Slerp(transform.localRotation, newRotation, .05f);
+
+            transform.Rotate(Vector3.forward * -50.0f * Time.deltaTime, Space.Self);
+            
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,5 +57,11 @@ public class Door : MonoBehaviour
                 _open = true;
             }
         }
+    }
+
+    public  void setIsOpen(bool doorStatus) 
+    {
+        Debug.Log("Hello");
+        _isOpen = doorStatus;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -69,6 +69,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void    OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Door")) {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Door door = other.gameObject.GetComponent<Door>();
+                door.setIsOpen(true);
+            }
+        }
+    }
+
+    // IEnumerator OpenDoor(Transform door, float nextRot)
+    // {
+    //     while(door.rotation.y < nextRot)
+    //     {
+    //         Debug.Log(door.rotation.y);
+    //         door.Rotate(0, Time.deltaTime * 100f, 0, Space.World);
+    //         yield return null;
+    //     }
+    // }
+
     public Dictionary<string, GameObject> GetPickUpObjects()
     {
         return _pickupObjects;
