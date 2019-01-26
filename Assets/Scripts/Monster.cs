abc1cefd2df63eea21077925d2636faed15589ad
Arthur Poisson <apoisson@student.42.fr>
2019-01-26 03:57:37 +0100

    Navmesh, test lumière, toussa

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour {
+
+  public GameObject Target;
+
+  private NavMeshAgent _navMeshAgent;
+
+  private void Awake () {
+    _navMeshAgent = GetComponent<NavMeshAgent>();
+  }
+
+  // Start is called before the first frame update
+  void Start() {
+    _navMeshAgent.SetDestination(Target.transform.position);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
