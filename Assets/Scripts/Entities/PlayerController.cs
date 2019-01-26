4a5c90971af13171ca4fc03a2da4f89acae47c59
qumaujean <qumaujea@student.42.fr>
2019-01-26 07:10:56 +0100

    Add pickUpObjects to player script

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -6,45 +6,53 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-  [SerializeField] private float _playerAccSpeed = 1f;
-  [SerializeField] private float _playerDecSpeed = 0.8f;
-	[SerializeField] private float _playerMaxSpeed = 5f;
+    [SerializeField] private float _playerAccSpeed = 1f;
+    [SerializeField] private float _playerDecSpeed = 0.8f;
+    [SerializeField] private float _playerMaxSpeed = 5f;
 
-  private Rigidbody _playerRigidBody;
-  private Vector3 _playerMovement;
+    private Rigidbody _playerRigidBody;
+    private Vector3 _playerMovement;
+    private Dictionary<string, GameObject> _pickupObjects;
 
-  private void Awake() {
-    _playerRigidBody = GetComponent<Rigidbody>();
-    _playerMovement = Vector3.zero;
-  }
-
-  private void Update() {
-    _playerMovement = new Vector3(
-      (Input.GetAxis("Horizontal") > 0.2f || Input.GetAxis("Horizontal") < -0.2f ? Input.GetAxis("Horizontal") : 0),
-      0,
-      (Input.GetAxis("Vertical") > 0.2f || Input.GetAxis("Vertical") < -0.2f ? Input.GetAxis("Vertical") : 0)
-    );
-
-    Debug.DrawLine(transform.position, transform.position - new Vector3(
-        _playerRigidBody.velocity.x,
-        0,
-        _playerRigidBody.velocity.z
-      ),
-    Color.blue);
-  }
-
-  private void FixedUpdate() {
-
-		if (Math.Abs(_playerMovement.x) > 0.02f || Math.Abs(_playerMovement.z) > 0.02f)
-			_playerRigidBody.velocity = _playerMovement * _playerAccSpeed;
-		else
-			_playerRigidBody.velocity *= _playerDecSpeed;
-		_playerRigidBody.velocity = Vector3.ClampMagnitude(_playerRigidBody.velocity, _playerMaxSpeed);
-  }
-
-  private void OnTriggerEnter(Collider other) {
-    if (other.gameObject.CompareTag("Pick Up")) {
-      other.gameObject.SetActive (false);
+    private void Awake()
+    {
+        _playerRigidBody = GetComponent<Rigidbody>();
+        _playerMovement = Vector3.zero;
+        _pickupObjects = new Dictionary<string, GameObject>();
     }
-  }
+
+    private void Update()
+    {
+        _playerMovement = new Vector3(
+          (Input.GetAxis("Horizontal") > 0.2f || Input.GetAxis("Horizontal") < -0.2f ? Input.GetAxis("Horizontal") : 0),
+          0,
+          (Input.GetAxis("Vertical") > 0.2f || Input.GetAxis("Vertical") < -0.2f ? Input.GetAxis("Vertical") : 0)
+        );
+
+        Debug.DrawLine(transform.position, transform.position - new Vector3(
+            _playerRigidBody.velocity.x,
+            0,
+            _playerRigidBody.velocity.z
+          ),
+        Color.blue);
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Math.Abs(_playerMovement.x) > 0.02f || Math.Abs(_playerMovement.z) > 0.02f)
+            _playerRigidBody.velocity = _playerMovement * _playerAccSpeed;
+        else
+            _playerRigidBody.velocity *= _playerDecSpeed;
+        _playerRigidBody.velocity = Vector3.ClampMagnitude(_playerRigidBody.velocity, _playerMaxSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            this._pickupObjects.Add(other.gameObject.name, other.gameObject);
+        }
+    }
 }
