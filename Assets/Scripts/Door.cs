9c1cffa3482025ca8268cd2659c1dcfc8962cfd7
Arthur Poisson <apoisson@student.42.fr>
2019-01-26 20:42:20 +0100

    Walk animation

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public GameObject Target;
-    private int _keyNumber;
-    private bool _open = true;
+  public GameObject Target;
+  private int _keyNumber;
+  private bool _open = true;
 
-    private GameObject _key;
+  private GameObject _key;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    GameObject cpy;
+
+    if (cpy = Target.transform.Find("Key").gameObject)
     {
-        GameObject cpy;
-
-        if (cpy = Target.transform.Find("Key").gameObject)
-        {
-            _open = false;
-            _key = cpy;
-        }
+      _open = false;
+      _key = cpy;
     }
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Player"))
     {
-
+      if (other.gameObject.GetComponent<PlayerController>().GetPickUpObjects().ContainsValue(this._key))
+      {
+        _open = true;
+      }
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            if (other.gameObject.GetComponent<PlayerController>().GetPickUpObjects().ContainsValue(this._key))
-            {
-                _open = true;
-            }
-        }
-    }
+  }
 }
