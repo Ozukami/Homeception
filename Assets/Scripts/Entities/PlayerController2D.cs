1ea7d1ca3fd7f8de4c539010671f292b39449a01
Julian Goldie <julian.goldie@epitech.eu>
2019-01-26 08:50:26 +0300

    lol le lookAt mouse

--- Assets/Scripts/Entities/PlayerController2D.cs
+++ Assets/Scripts/Entities/PlayerController2D.cs
@@ -29,6 +29,15 @@ public class PlayerController2D : MonoBehaviour
         0
       ),
     Color.blue);
+
+    Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+    Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+    transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+  }
+
+  float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+      return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
   }
 
   private void FixedUpdate() {
