8c687d77af875f58ee9711a2b50edfc690e14063
qumaujean <qumaujea@student.42.fr>
2019-01-27 14:17:22 +0100

    Merge fix

---
+++ Assets/Scripts/Lvl2Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl2Manager : MonoBehaviour
+{
+    public List<Pot> potList = new List<Pot>();
+    public List<Color> colorList = new List<Color>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Pot pot in List < Pot > potList)
+        {
+            if (!pot.isValid)
+                return;
+        }
+        GameObject player = GameObject.Find("Player");
+        GameObject keyFromRiddle = GameObject.Find("KeyFromRiddle");
+        player.PickUp(keyFromRiddle);
+        keyFromRiddle.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+
+    public int validColor;
+    public bool isValid;    // Start is called before the first frame update
+    public Lvl2Manager lvlManager;
+    public int index;
+
+    private SpriteRenderer rd;
+    void Start()
+    {
+        isValid = false;
+        rd = GetComponent<SpriteRenderer>();
+        rd.color = lvlManager.colorList[index];
+        Debug.Log(rd.color);
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("E"))
+            {
+                index++;
+                if (index > 4)
+                {
+                    index = 0;
+                    if (index == validColor)
+                        isValid = true;
+                }
+                rd.color = lvlManager.colorList[index];
+            }
+        }
+    }
+}
